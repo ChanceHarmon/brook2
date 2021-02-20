4801b00e645cdeb93726c2ede87a3588b74582e5
Chance Harmon <henrikillsit@gmail.com>
2021-02-20 15:46:20 -0800

    chipping away at refactor, taking a short break for family duties

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use stirct';
 
-const myFlies = []
-const travelFlies = [];
+Fly.myFlies = []
+Fly.travelFlies = [];
 
 $('#addFly').on('submit', handleSubmit);
 
@@ -14,16 +14,29 @@ function Fly(type, size, color) {
   this.type = type;
   this.size = size;
   this.color = color;
-  this.identifier = Date.now();
-  myFlies.push(this)
+  this.identifier = Fly.myFlies.length;
+  Fly.myFlies.push(this)
+}
+
+Fly.prototype.render = function () {
+
+
+  let $template = $('<li></li>')
+  let $liTemplate = $('#template').html()
+  $template.html($liTemplate)
+  $template.find('h3').text(this.type);
+  $template.find('h4').text(this.size);
+  $template.find('h5').text(this.color);
+  $('#my-flies').append($template)
+
 }
 
 function handleSubmit(event) {
   event.preventDefault();
-  console.log(event.target.flyType.value)
-  let returnFly = new Fly(event.target.flyType.value, event.target.size.value, event.target.color.value);
-
-  $('#my-flies').append(`<li><p>Type: ${returnFly.type},</p> <p>Size: ${returnFly.size},</p> <p>Color: ${returnFly.color}</p><button type="submit" value="${returnFly.identifier}">Add to Travel Box</button></li>`)
+  const $inputs = $('#addFly :input')
+  let returnFly = new Fly($inputs[0].value, $inputs[1].value, $inputs[2].value);
+  returnFly.render()
+  // $('#my-flies').append(`<li><p>Type: ${returnFly.type},</p> <p>Size: ${returnFly.size},</p> <p>Color: ${returnFly.color}</p><button type="submit" value="${returnFly.identifier}">Add to Travel Box</button></li>`)
   turnOnListner()
 
 }
